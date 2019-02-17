28b1bd1715d076788695d21e1988cef84fc373bf
Manuel Hernandez <manuelhe46@gmail.com>
2019-02-17 15:00:22 -0800

    add animation

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+var tl = new TimelineLite();
+tl.add( TweenLite.to("#person", 2, {x: 475, y:0   , ease:Power0.easeNone }));
+tl.add( TweenLite.to("#person", 1, {x: 475, y:-275, ease:Power0.easeNone }));
+tl.add( TweenLite.to("#person", 1, {x: 200, y:-275, ease:Power0.easeNone }));
+tl.add( TweenLite.to("#person", 1, {x: 475, y:-275, ease:Power0.easeNone }));
+tl.add( TweenLite.to("#person", 1, {x: 475, y:-375, ease:Power0.easeNone }));
+tl.add( TweenLite.to("#person", 1, {x: 300, y:-390, ease:Power0.easeNone }));
+tl.add( TweenLite.to("#person", 1, {x: 300, y:-485, ease:Power0.easeNone }));
+tl.add( TweenLite.to("#person", 1, {x:-150, y:-500, ease:Power0.easeNone }));
+tl.add( TweenLite.to("#person", 1, {x:-150, y:-400, ease:Power0.easeNone }));
+//tl.add( TweenLite.to("#person", 1, {x:300, y:-475 }));
+//tl.add( TweenLite.to("#person", 1, {x:600, y:400}));
+
