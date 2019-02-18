e4f1a69a0c6d3dbdfba61510a539d260978a7873
Manuel Hernandez <manuelhe46@gmail.com>
2019-02-17 16:18:49 -0800

    animate

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,30 @@
-var tl = new TimelineLite();
-tl.add( TweenLite.to("#person", 2, {x: 475, y:0   , ease:Power0.easeNone }));
-tl.add( TweenLite.to("#person", 1, {x: 475, y:-275, ease:Power0.easeNone }));
-tl.add( TweenLite.to("#person", 1, {x: 200, y:-275, ease:Power0.easeNone }));
-tl.add( TweenLite.to("#person", 1, {x: 475, y:-275, ease:Power0.easeNone }));
-tl.add( TweenLite.to("#person", 1, {x: 475, y:-375, ease:Power0.easeNone }));
-tl.add( TweenLite.to("#person", 1, {x: 300, y:-390, ease:Power0.easeNone }));
-tl.add( TweenLite.to("#person", 1, {x: 300, y:-485, ease:Power0.easeNone }));
-tl.add( TweenLite.to("#person", 1, {x:-150, y:-500, ease:Power0.easeNone }));
-tl.add( TweenLite.to("#person", 1, {x:-150, y:-400, ease:Power0.easeNone }));
-//tl.add( TweenLite.to("#person", 1, {x:300, y:-475 }));
-//tl.add( TweenLite.to("#person", 1, {x:600, y:400}));
 
+var path = [
+  { x: 475, y:    0 },
+  { x: 475, y: -275 },
+  { x: 200, y: -275 },
+  { x: 475, y: -275 },
+  { x: 475, y: -375 },
+  { x: 300, y: -390 },
+  { x: 300, y: -485 },
+  { x: 150, y: -500 },
+  { x: 150, y: -400 },
+  { x: 150, y: -525 },
+  { x: 150, y: -500 },
+  { x: 300, y: -500 },
+  { x: 300, y: -390 },
+  { x: 725, y: -390 },
+  { x: 725, y: -700 },
+  { x: 475, y: -700 },
+  { x: 475, y: -275 },
+  { x: 475, y:    0 },
+  { x:   0, y:    0 },
+] 
+
+
+var tl = new TimelineLite();
+
+for(i = 0; i < path.length; i++){
+  var step = path[i];
+  tl.add( TweenLite.to("#person", 2, {x:step.x, y:step.y, ease:Sine.easeOut }));
+}
